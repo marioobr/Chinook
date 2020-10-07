677c934e465b159ddd9d6f3ff8962a1eaf85311f
marioobr <obrlop@gmail.com>
2020-10-06 20:45:17 -0600

    Componiendo paginación

--- Chinook/Chinook/Controllers/CancionsController.cs
+++ Chinook/Chinook/Controllers/CancionsController.cs
@@ -20,9 +20,50 @@ namespace Chinook.Controllers
         }
 
         // GET: Cancions
-        public async Task<IActionResult> Index()
+        public async Task<IActionResult> Index(string CadenaBusq,
+            string sortOrder,
+            string currentFilter,
+            int? pageNumber)
         {
-            return View(await _context.Cancion.ToListAsync());
+            //Paginación
+            ViewData["CurrentSort"] = sortOrder;
+            //Ordenar
+            ViewData["NameSortParm"] = String.IsNullOrEmpty(sortOrder) ? "name_desc" : " ";
+
+            if (CadenaBusq != null)
+            {
+                pageNumber = 1;
+            }
+            else
+            {
+                CadenaBusq = currentFilter;
+            }
+            ViewData["CurrentFilter"] = CadenaBusq;
+
+            //Busqueda
+            var canciones = from c in _context.Cancion
+                            select c;
+
+            if (!String.IsNullOrEmpty(CadenaBusq))
+            {
+                canciones = canciones.Where(p => p.Nombre.Contains(CadenaBusq));
+            }
+
+
+            switch (sortOrder)
+            {
+                case "name_desc":
+                    canciones = canciones.OrderByDescending(o => o.Nombre);
+                    break;
+
+                default:
+                    canciones = canciones.OrderBy(p => p.Nombre);
+                    break;
+            }
+
+            int pageSize = 5;
+            //return View(await PaginatedList<Pelicula>.CreateAsync(peliculas.AsNoTracking(), pageNumber ?? 1, pageSize));
+            return View(await PaginatedList<Cancion>.CreateAsync(canciones.AsNoTracking(), pageNumber ?? 1, pageSize));
         }
 
         // GET: Cancions/Details/5
