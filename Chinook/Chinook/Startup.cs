9989331c6b43a33139cc07bcbe99d07758e81ab0
marioobr <obrlop@gmail.com>
2020-10-06 23:29:58 -0600

    Cambiando pagina Home

--- Chinook/Chinook/Startup.cs
+++ Chinook/Chinook/Startup.cs
@@ -55,7 +55,7 @@ namespace Chinook
             {
                 endpoints.MapControllerRoute(
                     name: "default",
-                    pattern: "{controller=Home}/{action=Index}/{id?}");
+                    pattern: "{controller=Artistas}/{action=Index}/{id?}");
             });
         }
     }
